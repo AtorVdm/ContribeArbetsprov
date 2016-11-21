8f9166c0dbcf753b59cefed1feaecdaab36b3bfb
Maksim Strutinskiy <atorvdm@gmail.com>
2016-11-21 20:30:26 +0100

    Initial commit. Spring Boot Hello World app.

---
+++ src/main/java/com/atorvdm/contribe/ContribeApplication.java
@@ -0,0 +1,11 @@
+package com.atorvdm.contribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContribeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ContribeApplication.class, args);
+	}
+}
